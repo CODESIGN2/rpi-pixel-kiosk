94c1cc3f4675faa071fb2b383377871909621105
Lewis Cowles <lewiscowles@me.com>
2016-10-02 06:55:40 +0100

    Added ability to disable screen blanking via FS

--- home/pi/start-kiosk.sh
+++ home/pi/start-kiosk.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+
+if [ -e '/boot/alwayson' ]; then
+  xset -dpms
+  xset s off
+fi
+
 # clean up if like me you just yank the power or ssh in and poweroff
 rm -rf ~/.config/chromium/Singleton*
 # Launch chromium in kiosk mode, incognito with a reputable, well-designed url
