3a7d188e39514e500c68b339e77ef3e80c5c02f3
Lewis Cowles <lewiscowles@me.com>
2017-10-10 21:08:25 +0100

    move URL into a environment variable
    Fixes #2

--- home/pi/start-kiosk.sh
+++ home/pi/start-kiosk.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+if [ -z "$KIOSKURL" ]; then
+    KIOSKURL="https://www.codesign2.co.uk"
+fi
+
 if [ -e '/boot/alwayson' ]; then
   xset -dpms
   xset s off
@@ -8,4 +12,4 @@ fi
 # clean up if like me you just yank the power or ssh in and poweroff
 rm -rf ~/.config/chromium/Singleton*
 # Launch chromium in kiosk mode, incognito with a reputable, well-designed url
-chromium-browser --kiosk --incognito https://www.codesign2.co.uk
+chromium-browser --kiosk --incognito $KIOSKURL
